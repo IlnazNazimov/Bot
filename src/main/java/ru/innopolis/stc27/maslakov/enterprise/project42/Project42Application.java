9e7cff3605924f422c8b1baacea2d7b045a3562f
Ilnaz Nazimov <nz.il@yandex.ru>
2020-10-06 18:13:38 +0300

    First commit in Bot

--- src/main/java/ru/innopolis/stc27/maslakov/enterprise/project42/Project42Application.java
+++ src/main/java/ru/innopolis/stc27/maslakov/enterprise/project42/Project42Application.java
@@ -2,12 +2,22 @@ package ru.innopolis.stc27.maslakov.enterprise.project42;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 @SpringBootApplication
 public class Project42Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Project42Application.class, args);
+        //SpringApplication.run(Project42Application.class, args);
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
     }
 
 }
