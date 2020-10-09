e0d8dcdb52ce9fe325ed6c3a73e81ea41e6c92f9
Ilnaz Nazimov <nz.il@yandex.ru>
2020-10-09 12:37:55 +0300

    1277.Создать простейший эхо-бот в телеграм. Добавил кнопки

--- src/main/java/ru/innopolis/stc27/maslakov/enterprise/project42/Bot.java
+++ src/main/java/ru/innopolis/stc27/maslakov/enterprise/project42/Bot.java
@@ -3,9 +3,15 @@ package ru.innopolis.stc27.maslakov.enterprise.project42;
 import org.telegram.telegrambots.api.methods.BotApiMethod;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Bot extends TelegramWebhookBot {
     private String webHookPath;
@@ -17,6 +23,44 @@ public class Bot extends TelegramWebhookBot {
         super(botOptions);
     }
 
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId());
+        if (update.getMessage() != null && update.getMessage().hasText()) {
+            try {
+                sendButton(sendMessage);
+                if (update.getMessage().getText().equals("Меню")) {
+                    sendMessage.setText("1.Хлеб\n2.Вода");
+                } else if (update.getMessage().getText().equals("Забронировать стол")) {
+                    sendMessage.setText("Сори, все столики забронированы");
+                } else sendMessage.setText("Не валяй дурака");
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    private void sendButton(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardRow = new KeyboardRow();
+
+        keyboardRow.add("Меню");
+        keyboardRow.add("Забронировать стол");
+        keyboardRowList.add(keyboardRow);
+
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+    }
 
     @Override
     public String getBotToken() {
@@ -32,23 +76,6 @@ public class Bot extends TelegramWebhookBot {
     public String getBotPath() {
         return webHookPath;
     }
-
-    @Override
-    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        if (update.getMessage() != null && update.getMessage().hasText()) {
-            long chat_id = update.getMessage().getChatId();
-
-
-            try {
-                execute(new SendMessage(chat_id, "Hi " + update.getMessage().getText()));
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return null;
-    }
-
 
     public void setWebHookPath(String webHookPath) {
         this.webHookPath = webHookPath;
--- src/main/java/ru/innopolis/stc27/maslakov/enterprise/project42/Project42Application.java
+++ src/main/java/ru/innopolis/stc27/maslakov/enterprise/project42/Project42Application.java
@@ -2,11 +2,10 @@ package ru.innopolis.stc27.maslakov.enterprise.project42;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
-public class Project42Application extends SpringBootServletInitializer {
+public class Project42Application {
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
